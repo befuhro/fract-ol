14ec66861ab9c72262180e18c36b024d6c830046
Fuhro Benjamin <befuhro@z1r5p2.le-101.fr>
2018-02-15 18:01:02 +0100

    zoom ok

--- srcs/zoom.c
+++ srcs/zoom.c
@@ -6,42 +6,28 @@
 /*   By: befuhro <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/02/12 20:13:21 by befuhro      #+#   ##    ##    #+#       */
-/*   Updated: 2018/02/14 23:31:47 by befuhro     ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/02/15 16:29:46 by befuhro     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "fract-ol.h"
-#include <stdio.h>
 
 void	zoom(t_all *all, int x, int y)
 {	
 	double	ratio;
 
 	ratio = (all->fract->maxX - all->fract->minX) * (100 / 5.2);
-		(void)x;
-	(void)y;
-
-
 	
-	all->fract->minX += (x - (WIDTH / 2)) * (ratio / 260000);
-	all->fract->maxX +=  (x - (WIDTH / 2)) * (ratio / 260000);
-
-	all->fract->minY +=  (y - (HEIGHT / 2)) * (ratio / 260000);
-	all->fract->maxY +=  (y - (HEIGHT / 2)) * (ratio / 260000);
+	all->fract->minX += (x - (WIDTH / 2)) * (ratio / 284150);
+	all->fract->maxX += (x - (WIDTH / 2)) * (ratio / 284150);
+	all->fract->minY += (y - (HEIGHT / 2)) * (ratio / 238000);
+	all->fract->maxY += (y - (HEIGHT / 2)) * (ratio / 238000);
 
 	all->fract->minX += ratio / (1000 / 2.4);
 	all->fract->maxX -= ratio / (1000 / 2.4);
 	all->fract->minY += ratio / (1000 / 1.5);
 	all->fract->maxY -= ratio / (1000 / 1.5);
-
-
-
-
-	printf("minX\t\tmaxX\t\tminY\t\tmaxY\n___________________________________________\n%f\t%f\t%f\t%f\n\n\n\n",
-		all->fract->minX,  all->fract->maxX,  all->fract->minY, all->fract->maxY);
-
-
 }
 
 void	unzoom(t_all *all, int x, int y)
@@ -50,17 +36,13 @@ void	unzoom(t_all *all, int x, int y)
 
 	ratio = (all->fract->maxX - all->fract->minX) * (100 / 5.2);
 	
-	(void)x;
-	(void)y;
+	all->fract->minX -= (x - (WIDTH / 2)) * (ratio / 284150);
+	all->fract->maxX -= (x - (WIDTH / 2)) * (ratio / 284150);
+	all->fract->minY -= (y - (HEIGHT / 2)) * (ratio / 238000);
+	all->fract->maxY -= (y - (HEIGHT / 2)) * (ratio / 238000);
 
-//	all->fract->minX -= (x - (WIDTH / 2)) * (ratio / 100000);
-//	all->fract->maxX -=  (x - (WIDTH / 2)) * (ratio / 100000);
-
-//	all->fract->minY -=  (y - (HEIGHT / 2)) * (ratio / 100000);
-//	all->fract->maxY -=  (y - (HEIGHT / 2)) * (ratio / 100000);
-
-	all->fract->minX *= 1.05;
-	all->fract->maxX *= 1.05;
-	all->fract->minY *= 1.05;
-	all->fract->maxY *= 1.05;
+	all->fract->minX -= ratio / (1000 / 2.4);
+	all->fract->maxX += ratio / (1000 / 2.4);
+	all->fract->minY -= ratio / (1000 / 1.5);
+	all->fract->maxY += ratio / (1000 / 1.5);
 }
